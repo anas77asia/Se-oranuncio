133cbe33b9da36f360731c8eb4ede7a6092694a3
Carlos <carlos.mejia@est.iudigital.edu.co>
2020-10-30 01:59:12 -0500

    Nuevo proyecto

---
+++ index.php
@@ -0,0 +1,129 @@
+
+<!DOCTYPE html>
+<html>
+    <!--CABECERA-->
+    <head>
+        <meta charset="UTF-8">
+        <title>  Señor Anuncio - Pautas Baratas en Internet</title>
+        <link rel="shortcut icon" type="image/png" href="imagenes/logo-favicon.png">
+        <link rel="stylesheet" href="css/estilos.css">
+
+    </head>
+    <body class="container">
+        <div>
+             <!--ENCABEZADO-->
+            <header class="superior">
+                <div class="logo-señor-anuncio">
+                     <!--LOGO-->
+                    <img src="imagenes/logo-señor-anuncio.png" width="30%">
+                </div>
+                 <!--MENU NAVEGACIÓN-->
+                <nav class="menu">
+                    <ul class="opciones">
+                        <li><a href="">Inicio</a></li>
+                        <li><a href="">Servicios</a></li>
+                        <li><a href="">Noticias</a></li>
+                        <li><a href="">Contacto</a></li>
+                        <li><a href="">Blog</a></li>
+                        
+                    </ul>
+                </nav>
+            </header>
+            <section>
+                <ul>
+                    
+                   <!-- SLIDERS -->
+            <div class="slider">
+                <ul>
+                    <li> <img src="imagenes/slider01.jpg.jpeg" width="100%" height="500"></li>
+                    <li><img src="imagenes/slider02.jpg.jpeg" width="100%" height="500"></li>
+                    <li><img src="imagenes/slider03jpg.jpeg" width="100%" height="500"></li>
+                    <li><img src="imagenes/slider04.jpeg.jpeg" width="100%" height="500"></li>
+                </ul>
+            </div>
+                             <!-- PRODUCTOS -->
+
+            <section class="cuerpo">
+                    <h1 class="titulo">NUESTROS SERVICIOS</h1>
+
+
+                    <!-- PRODUCTO O SERVICIO 1 -->
+                    <article class="caja">
+                        <div class="subtitulo">
+                            <img src="imagenes/servicio01.jpg" width="100%" class="efecto" height="200">
+                            <h3 class="servicios">ANUNCIOS PROMOCIONALES</h3>
+                        </div>
+                        <div>
+                            <p class="parrafo">
+                                Agencia de Marketing digital con las mejores estrategias en: Social Media, SEO, SEM,
+                                Branding y Publicidad
+                            </p>
+                            <center><input type="submit" value="Leer mas" class="boton"></center>
+                    </article>
+                    <article>
+                        <!-- PRODUCTO O SERVICIO 2 -->
+                        <article class="caja">
+                            <div class="subtitulo">
+                                <img src="imagenes/servicio02.png" width="100%" class="efecto" height="200">
+                                <h3 class="servicios">POSICONAMIENTO SEO</h3>
+                            </div>
+                            <div>
+                                <p class="parrafo">
+                                    Creacion de contenidos animados o estaticos, con las mejores pautas y campañas en redes
+                                    sociales.
+                                </p>
+                                <center><input type="submit" value="Leer mas" class="boton"> </center>
+
+                            </div>
+                        </article>
+                        <!-- PRODUCTO O SERVICIO 3 -->
+                        <article class="caja">
+                            <div class="subtitulo">
+                                <img src="imagenes/servicio03.jpg" width="100%" class="efecto" height="200">
+                                <h3 class="servicios">DESARROLLO WEB & APLICAIONES MÓVILES</h3>
+                            </div>
+                            <div>
+                                <p class="parrafo">
+                                    Creamos tu sitio web o aplicacion movil adaptandonos a tus necesidades y a las del
+                                    mercado.
+                                </p>
+    
+                                <center><input type="submit" value="Leer mas" class="boton"></center>
+                            </div>
+                        </article>
+               
+                             
+                    </section>
+                    <!-- LOCALIZACION -->
+            <section class="localizacion">
+                    <h1 class="titulo">Encuentranos</h1>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4577946665186!2d-75.60140714888824!3d6.203181728501122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e46820b66d18b5b%3A0x37faa1e22cbac389!2sCl.%209%20Sur%20%2379a20%2C%20Medell%C3%ADn%2C%20Antioquia%2C%20Colombia!5e0!3m2!1ses!2suk!4v1603687790637!5m2!1ses!2suk" width="1790" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </section>
+
+
+                <!-- PIE DE PAGINA -->
+            <footer class="pie">
+                    <!--INFORMACION EMPRESA-->
+                    <div class="izquierda">
+                        <center><img src="imagenes/logo-señor-anuncio.png" width="20%"></center>
+                        <p class="abajo">Señor Anuncio se esfuerza para darte el mejor servicio.</p>
+                    </div>
+    
+                    <div class="derecha">
+                        <!--REDES SOCIALES-->
+                        <p class="siguenos">Siguenos en:</p>
+                        <br>
+                        <center>
+                            <a href="https://www.facebook.com" target="blank"><img src="imagenes/facebook.png"
+                                    class="redes"></a>
+                            <a href="https://www.instagram.com" target="blank"><img
+                                    src="imagenes/instagram.png" class="redes"></a>
+                        </center>
+                        <br>
+                        
+                    </div>
+                </footer>
+        </div>
+    </body>
+
+</html>
